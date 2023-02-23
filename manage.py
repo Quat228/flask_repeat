3078a0a4a24bef632992914ab31ccb86b1e51951
Quat228 <zharkulov04@mail.ru>
2023-02-23 21:03:47 +0600

    added manage.py

---
+++ manage.py
@@ -0,0 +1,11 @@
+from app import app, db
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
+
+
+# git init
+# git status
+# git add manage.py
